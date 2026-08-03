1fd16c5b04aa479fa4fb701be8f70b9c83904d72
IT-CodeUser <donkeykongking50@gmail.com>
2026-08-03 19:04:15 +0000

    Add files via upload

---
+++ day_01_basics.py
@@ -0,0 +1,15 @@
+# --- LESSON 1: VARIABLES AND STRINGS ---
+name = "My G"
+salary = 50000.
+print(salary)
+print(name)
+
+name = 9000
+salary = "lost"
+print("salary")
+print("name")
+
+salary = "Host"
+salary = "67844"
+print(salary)
+print(name)
