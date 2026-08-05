424574a0b990a922e19bba20bf7df6c72fa8f538
IT-CodeUser <donkeykongking50@gmail.com>
2026-08-05 19:17:55 +0000

    Add files via upload

---
+++ day_03_logic.py
@@ -0,0 +1,29 @@
+for Hoop in range(1 , 11):
+    print(Hoop)
+
+
+
+for Num in reversed(range(1 ,11)):
+    print(Num)
+
+print("Have a good day")
+print("I'm ready for a job interview")
+
+
+
+for Y in range(1 ,11 ,2):
+    print(Y)
+
+
+for x in range(1 ,21):
+    if x == 13:
+       break
+    else:
+        print(x)
+
+
+for x in range(1 ,200):
+    if x == 100:
+       continue
+    else:
+        print(x)
