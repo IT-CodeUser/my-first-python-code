095add30d1e8122dc3bbc53c229d02c05012a4f8
IT-CodeUser <donkeykongking50@gmail.com>
2026-09-08 21:02:47 +0000

    Add files via upload

---
+++ Day_14_Operators.py
@@ -0,0 +1,15 @@
+# Define variables
+a = 15
+b = 4
+
+# Perform calculations
+addition = a + b        # 15 + 4
+subtraction = a - b     # 15 - 4
+multiplication = a * b  # 15 * 4
+division = a / b        # 15 / 4
+
+# Print results
+print(f"Addition:       {a} + {b} = {addition}")
+print(f"Subtraction:    {a} - {b} = {subtraction}")
+print(f"Multiplication: {a} * {b} = {multiplication}")
+print(f"Division:       {a} / {b} = {division}")
