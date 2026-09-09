ba68effb92dfc821d561fbf928b91183bf418925
IT-CodeUser <donkeykongking50@gmail.com>
2026-09-09 18:46:05 +0000

    Delete Day_15_if, elif, and else statements(again).py

--- Day_15_if, elif, and else statements(again).py
+++
@@ -1,9 +0,0 @@
-if condition1:
-    # Executes if condition1 is True
-    print("Condition 1 is met")
-elif condition2:
-    # Executes if condition1 is False AND condition2 is True
-    print("Condition 2 is met")
-else:
-    # Executes only if all previous conditions are False
-    print("No conditions were met")
